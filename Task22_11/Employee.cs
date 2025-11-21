f3421a26663c694b5c60c3856706a06960a630f9
Gunel2727 <gunelemu@code.edu.az>
2025-11-22 01:54:36 +0400

    task is added

---
+++ Task22_11/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task22_11
+{
+    internal class Employee
+    {
+        public string Name { get; set; }
+        public string SurName { get; set; }
+        public DateTime BirthDay { get; set; }
+        public int Salary { get; set; }
+        public Employee(string name, string surname,DateTime birthday, int salary)
+        {
+            Salary = salary;
+            Name = name;
+            SurName = surname;
+            BirthDay = birthday;
+        }
+
+    }
+}
---
+++ Task22_11/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Task22_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Elave etmek isdeyiniz isci sayi: ");
+            int countemployee=Convert.ToInt32(Console.ReadLine());
+            Employee[] employees =new Employee [countemployee];
+            for (int i = 0; i <countemployee; i++)
+            {
+                Console.WriteLine("Enter Name: ");
+                string name= Console.ReadLine();
+                Console.WriteLine("Enter SurName: ");
+                string surname= Console.ReadLine();
+                Console.WriteLine("Enter Birthday: ");
+                string input = Console.ReadLine();
+                DateTime birthday = DateTime.Parse(input);
+                Console.WriteLine("Enter Salary: ");
+                int salary= Convert.ToInt32(Console.ReadLine());
+                employees[i]=new Employee(name,surname,birthday,salary);
+            }
+
+            Console.WriteLine("Date 1: ");
+            DateTime datenum1 = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("Date 2: ");
+            DateTime datenum2 = DateTime.Parse(Console.ReadLine());
+            int result=CountEmployees(employees, datenum1, datenum2);
+            Console.WriteLine(result);
+        }
+        public static int CountEmployees(Employee[] employees, DateTime date1, DateTime date2)
+        {
+            int count = 0;
+
+            foreach (Employee e in employees)
+            {
+                if (e.BirthDay > date1 && e.BirthDay < date2 && e.Salary > 2000)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+    }
+}
